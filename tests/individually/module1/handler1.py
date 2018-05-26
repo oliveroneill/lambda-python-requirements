a3afe6ca29218c7d154da0716e3cc37ea959129f
Oliver <oliveroneill04@gmail.com>
2018-05-26 20:17:41 +1000

    Cleanup

--- tests/individually/module1/handler1.py
+++
@@ -1,6 +0,0 @@
-import boto3
-
-def hello(event, context):
-    return {
-        'status': 200,
-    }
--- tests/individually/module2/handler2.py
+++
@@ -1,6 +0,0 @@
-import flask
-
-def hello(event, context):
-    return {
-        'status': 200,
-    }
